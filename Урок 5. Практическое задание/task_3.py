6e92aca85ddccd9a3bfdd265e9c9403230cb1b60
michael_kurashev <kurashevmichael@gmail.com>
2021-10-26 19:25:30 +0300

    add file 1-4

--- Урок 5. Практическое задание/task_3.py
+++ Урок 5. Практическое задание/task_3.py
@@ -11,3 +11,70 @@ deque – это обобщение стеков и очередей.
 
 В первую очередь необходимо выполнить замеры для ф-ций appendleft, popleft, extendleft дека и для их аналогов у списков.
 """
+
+# Задача 1
+
+from collections import deque
+from timeit import timeit
+
+simple_list = []
+deq_obj = deque()
+
+def func_1():
+    for i in 'bcddddddd':
+        simple_list.append(i)
+
+def func_2():
+    for i in 'bcddddddd':
+        deq_obj.append(i)
+
+print(timeit(stmt="func_1()", setup="from __main__ import func_1",  number=10000))
+# 0.0101819
+print(timeit(stmt="func_2()", setup="from __main__ import func_2",  number=10000))
+# 0.008375500000000001
+
+"""
+Заполнение очереди происходит быстрее, так как операции добавления имеют сложность O(1)
+"""
+
+# Задача 2
+
+def func_3():
+    simple_list.insert(0, "1")
+
+def func_4():
+    deq_obj.appendleft("1")
+
+def func_5():
+    a = simple_list.pop()
+    return a
+
+def func_6():
+    a = deq_obj.popleft()
+    return a
+
+def func_7():
+    simple_list.extend(['2', '3'])
+
+def func_8():
+    deq_obj.extendleft('23')
+
+print(timeit(stmt="func_3()", setup="from __main__ import func_3",  number=10000))
+# 0.9187539
+print(timeit(stmt="func_4()", setup="from __main__ import func_4",  number=10000))
+# 0.0015555999999999903
+print(timeit(stmt="func_5()", setup="from __main__ import func_5",  number=10000))
+# 0.0015081000000000122
+print(timeit(stmt="func_6()", setup="from __main__ import func_6",  number=10000))
+# 0.0014278999999999265
+print(timeit(stmt="func_7()", setup="from __main__ import func_7",  number=10000))
+# 0.002538200000000046
+print(timeit(stmt="func_8()", setup="from __main__ import func_8",  number=10000))
+# 0.002163000000000026
+
+
+"""
+Операции вставки и удаления сначала и конца у очередей происходят быстрее, так как сложность О(1). Однако все что
+касается случайного доступа элемента, поиска по списку определенного элемента скорость списка будет быстрее. Это мы
+видим в примерах выше
+"""
--- Урок 5. Практическое задание/task_4.py
+++ Урок 5. Практическое задание/task_4.py
@@ -6,3 +6,38 @@
 
 И есть ли смысл исп-ть OrderedDict в Python 3.6 и более поздних версиях?
 """
+
+from timeit import timeit
+import collections
+
+dict_one = {1: 'hello', 2: 'vvv', 3: 'ddd'}
+dict_ordereddict = collections.OrderedDict([(1, 'hello'), (2, 'vvv'), (3, 'ddd')])
+
+def func_1():
+    for i in range(4,8):
+        dict_one[i] = 'hello'
+
+def func_2():
+    for i in range(4,8):
+        dict_ordereddict[i] = 'hello'
+
+def func_3():
+    return dict_one[1]
+
+def func_4():
+    return dict_ordereddict[1]
+
+print(timeit(stmt="func_1()", setup="from __main__ import func_1",  number=10000))
+# 0.005635399999999999
+print(timeit(stmt="func_2()", setup="from __main__ import func_2",  number=10000))
+# 0.011919599999999995
+print(timeit(stmt="func_3()", setup="from __main__ import func_3",  number=10000))
+# 0.0011161999999999977
+print(timeit(stmt="func_4()", setup="from __main__ import func_4",  number=10000))
+# 0.001102800000000001
+
+"""
+В случае заполнения словаря коллекция ordereddict была медленнее, чем словарь. В случае получения ключа незначительно
+быстрее оказался ordereddict.
+Смысла использовать ordereddict в Python 3.6 и выше не вижу, так как он медленнее, а в новых версиях словарь упорядочен
+"""
